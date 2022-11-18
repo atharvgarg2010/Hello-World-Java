23789b1bcd9a849c40457c61260292bdae85c569
Atharv garg <102018708+atharvgarg2010@users.noreply.github.com>
2022-11-18 09:07:50 +0530

    Add files via upload

---
+++ Atharv.java
@@ -0,0 +1,19 @@
+
+class Atharv2{
+    public static void main(String args[]){
+        int b = 23;
+        int a[]=new int[5];//declaration and instantiation  
+        a[0]=10;//initialization  
+        a[1]=20;  
+        a[2]=70;  
+        a[3]=40;  
+        a[4]=50;  
+        String f = "Atharv";
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println(a);
+        System.out.println(f);
+    }
+}
